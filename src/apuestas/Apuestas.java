f49582d84cbf7335a1d82d9b338e2223efecfe67
Emiliano.m.s <cabeemil86@gmail.com>
2023-02-22 12:10:15 +0100

    refactorizacion EXTRAER METODO

--- src/apuestas/Apuestas.java
+++ src/apuestas/Apuestas.java
@@ -12,13 +12,17 @@ public class Apuestas {
         int mi_dinero;
 
         laApuesta = new Apuesta(1000, 4, 2);
+        operativa_Apuesta(laApuesta);
+    }
+
+    private static void operativa_Apuesta(Apuesta laApuesta) {
+        int mi_dinero;
         try {
             System.out.println("Apostando...");
             laApuesta.apostar(25);
         } catch (Exception e) {
             System.out.println("Fallo al realizar la Apuesta");
         }
-
         try {
             System.out.println("Intento cobrar apuesta segun el resultado del partido");
             laApuesta.cobrar_apuesta(2, 3);
